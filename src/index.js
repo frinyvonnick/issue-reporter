d21ee48eb4a4b9f3751d9d1f4227302c3526ada4
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-12-03 21:04:49 +0100

    :construction: Handle different workflows

--- src/index.js
+++ src/index.js
@@ -3,50 +3,66 @@ const Handlebars = require('handlebars')
 const issueTemplate = require('./issue-template')
 const newGithubIssueUrl = require('new-github-issue-url')
 const terminalLink = require('terminal-link')
+const clipboardy = require('clipboardy')
+const envCi = require('env-ci')
 
-async function errorReporter (e) {
+async function makeReportFromError (e) {
   const envInfosResult = await envinfo.run({
     System: ['OS'],
   }, { markdown: true })
 
   return {
-    sections: [
-      {
-        title: 'Stacktrace',
-        stack: e.stack,
-      },
-      {
-        title: 'Environment',
-        markdown: envInfosResult
-      }
-    ]
+    error: {
+      title: 'Stacktrace',
+      stack: e.stack,
+    },
+    environment: {
+      title: 'Environment',
+      markdown: envInfosResult
+    }
   }
 }
 
-function transformToMarkdown(errorReport) {
+function makeIssueBodyFromReport(errorReport) {
   const compiler = Handlebars.compile(issueTemplate)
   return compiler({
-    error: errorReport.sections[0],
-    environment: errorReport.sections[1],
+    error: errorReport.error,
+    environment: errorReport.environment,
   })
 }
 
-function outputGenerator(user, repo, markdown) {
-  const url = newGithubIssueUrl({
-    user,
-    repo,
-    body: markdown
-  });
+function generateOutputFromIssueBody(user, repo, markdown) {
+  let errorMsg = `Error: Whoops, something went wrong, please click on the following link to `
+  const repoInfos = { user, repo }
+  const { isCi } = envCi()
 
-  const link = terminalLink('create an issue', url)
-
-  const errorMsg = `Error: Whoops, something went wrong, please click on the following link to ${link}`
+  if (isCi) {
+    const link = newGithubIssueUrl({
+      ...repoInfos,
+      body: 'Please paste your error report here.',
+    });
+    errorMsg += `create an issue ${link}.\n\n${markdown}`
+  } else if (terminalLink.isSupported) {
+    const url = newGithubIssueUrl({
+      ...repoInfos,
+      body: markdown,
+    });
+    const link = terminalLink('create an issue', url)
+    errorMsg += `${link}.`
+  } else {
+    const link = newGithubIssueUrl({
+      ...repoInfos,
+      body: 'An error report is in your clipboard. Please paste it here.',
+    });
+    clipboardy.writeSync(markdown)
+    errorMsg += `create an issue ${link}. An error report has been copied to your clipboard.`
+  }
 
   console.log(errorMsg)
 }
 
 module.exports = {
-  errorReporter,
-  transformToMarkdown,
-  outputGenerator
+  makeReportFromError,
+  makeIssueBodyFromReport,
+  generateOutputFromIssueBody
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,15 +1,16 @@
 const envinfo = require('envinfo')
-const { errorReporter, transformToMarkdown, outputGenerator } = require('./index')
+const clipboardy = require('clipboardy')
+const { makeReportFromError, makeIssueBodyFromReport, generateOutputFromIssueBody } = require('./index')
 
 describe('Error reporter', () => {
-  describe('Core infos', () => {
+  describe('makeReportFromError', () => {
     it('should return a correctly formatted stacktrace section', async () => {
       const error = new Error('Some error')
       
-      const result = await errorReporter(error)
+      const result = await makeReportFromError(error)
   
-      expect(result.sections[0].title).toEqual('Stacktrace')
-      expect(result.sections[0].stack).toEqual(error.stack)
+      expect(result.error.title).toEqual('Stacktrace')
+      expect(result.error.stack).toEqual(error.stack)
     })
   
     it('should return a correctly formatted env infos section', async () => {
@@ -18,44 +19,68 @@ describe('Error reporter', () => {
         System: ['OS'],
       }, { markdown: true })
       
-      const result = await errorReporter(error)
+      const result = await makeReportFromError(error)
   
-      expect(result.sections[1].title).toEqual('Environment')
-      expect(result.sections[1].markdown).toEqual(envInfosResult)
+      expect(result.environment.title).toEqual('Environment')
+      expect(result.environment.markdown).toEqual(envInfosResult)
     })
   })
   
-  describe('Markdown formatter', () => {
+  describe('makeIssueBodyFromReportu', () => {
     it('should format a json error as markdown', () => {
       const errorReport = {
-        sections: [
-          {
-            title: 'Stacktrace',
-            stack: 'Stack'
-          },
-          {
-            title: 'Environment',
-            markdown: 'Environment markdown'
-          }
-        ]
+        error: {
+          title: 'Stacktrace',
+          stack: 'Stack'
+        },
+        environment: {
+          title: 'Environment',
+           markdown: 'Environment markdown'
+        }
       }
 
-      const result = transformToMarkdown(errorReport)
+      const result = makeIssueBodyFromReport(errorReport)
 
       expect(result).toMatchSnapshot()
     })
   })
 
-  describe('Output generator', () => {
+  describe('generateOutputFromIssueBody', () => {
     it('should print a link to open the page that creates a new issue filled with error report', () => {
       jest.spyOn(global.console, 'log')
       const markdown = `## Report
       
 Awesome report`
 
-      outputGenerator('frinyvonnick', 'gitmoji-changelog', markdown)
+      generateOutputFromIssueBody('frinyvonnick', 'gitmoji-changelog', markdown)
 
       expect(console.log).toHaveBeenCalledWith(expect.stringContaining(`Error: Whoops, something went wrong, please click on the following link to create an issue`))
+    })
+
+    it('should copy issue body to clipboard when hyperlinks are not supported by the terminal', () => {
+      const markdown = `## Report
+      
+Awesome report`
+
+      generateOutputFromIssueBody('frinyvonnick', 'gitmoji-changelog', markdown)
+
+      const clipboardContent = clipboardy.readSync()
+
+      expect(console.log).toHaveBeenCalledWith(expect.stringContaining(`Error: Whoops, something went wrong, please click on the following link to create an issue https://github.com/frinyvonnick/gitmoji-changelog/issues/new?body=An+error+report+is+in+your+clipboard.+Please+paste+it+here.. An error report has been copied to your clipboard.`))
+      expect(clipboardContent).toEqual(markdown)
+    })
+
+    it('should write issue body directly to stdout if the function is called in TTY', () => {
+      const markdown = `## Report
+      
+Awesome report`
+
+      process.env.CI = true
+
+      generateOutputFromIssueBody('frinyvonnick', 'gitmoji-changelog', markdown)
+
+      expect(console.log).toHaveBeenCalledWith(expect.stringContaining(`Error: Whoops, something went wrong, please click on the following link to create an issue https://github.com/frinyvonnick/gitmoji-changelog/issues/new?body=Please+paste+your+error+report+here..`))
+      expect(console.log).toHaveBeenCalledWith(expect.stringContaining(markdown))
     })
   })
 })
